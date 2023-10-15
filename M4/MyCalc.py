517a48ab86172266557f1919cabb840b57b75aa1
Bhavya215 <shahbhavya215@gmail.com>
2023-10-15 12:44:17 -0400

    adding final code and test cases

--- M4/MyCalc.py
+++ M4/MyCalc.py
@@ -1,73 +1,80 @@
-import operator  # https://docs.python.org/3/library/operator.html
-
-# TODO idea
-# 1) Allow longer equations
-# 2) expand math functions
-# 3) Separate calculator answers
-# 4) Support config options (i.e., rounding logic, answer conversion)
-# 5) Persist last answer between application restarts
-# 6) Record eq history
-
-
 class MyCalc:
-    ans = 0  # todo, class vs static vs instance
-    ops = {'+': operator.add,
-           '-': operator.sub,
-           '*': operator.mul,
-           'x': operator.mul,
-           '/': operator.truediv}
+    ans = 0
 
-
-    @staticmethod
-    def _is_float(val):
+    def _is_float(self, val):
         try:
-            float(val)
+            val = float(val)
             return True
         except:
             return False
 
-    @staticmethod
-    def _is_int(val):
+    def _is_int(self, val):
         try:
-            int(val)
+            val = int(val)
             return True
         except:
             return False
 
-    @staticmethod
-    def _as_number(val):
-        if MyCalc._is_int(val):
+    def _as_number(self, val):
+        if self._is_int(val):
             return int(val)
-        elif MyCalc._is_float(val):
+        elif self._is_float(val):
             return float(val)
         else:
             raise Exception("Not a number")
 
-    def calc(self, num1, op, num2):
-        # map characters to operator function references
-        # https://stackoverflow.com/a/18591880
-        # print("num1: " + str(num1))
-        # print("num2: " + str(num2))
+    def add(self, num1, num2):
+        """Name: Bhavya Shah; UCID: bs635; Date: 15 October, 2023"""
+        """This function adds two numbers or utilizes the previous answer (ans) and add it to the number."""
         if num1 == "ans":
-            return self.calc(self.ans, op, num2)
+            return self.add(self.ans, num2)
         else:
             num1 = self._as_number(num1)
             num2 = self._as_number(num2)
-            self.ans = MyCalc.ops[op](num1, num2)
+            self.ans = num1+num2
         return self.ans
 
-class AdvMyCalc(MyCalc):
-    def __init__(self):
-        super().ops["**"] = operator.pow
-        super().ops["//"] = operator.floordiv
-        super().ops["%"] = operator.mod
+    def sub(self, num1, num2):
+        """Name: Bhavya Shah; UCID: bs635; Date: 15 October, 2023"""
+        """This function substracts two numbers or utilizes the previous answer (ans) and substract it to the num."""
+        if num1 == "ans":
+            return self.sub(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1-num2
+        return self.ans
+
+    def mult(self, num1, num2):
+        """Name: Bhavya Shah; UCID: bs635; Date: 15 October, 2023"""
+        """This function multiplies two numbers or utilizes the previous answer (ans) and multiply it to the number."""
+        if num1 == "ans":
+            return self.mult(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1*num2
+        return self.ans
+
+    def div(self, num1, num2):
+        """Name: Bhavya Shah; UCID: bs635; Date: 15 October, 2023"""
+        """This function divides two numbers or utilizes the previous answer (ans) and divide it to the number."""
+        if num1 == "ans":
+            return self.div(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            if num2 == 0:
+                print("Can't divide by zero, sorry")
+            else:
+                self.ans = num1/num2
+        return self.ans
 
 
 if __name__ == '__main__':
     is_running = True
     iSTR = input("Are you ready?")
-    #calc = MyCalc()
-    calc = AdvMyCalc()
+    calc = MyCalc()
     print(calc)
     if iSTR == "yes":
         while is_running:
@@ -76,16 +83,29 @@ if __name__ == '__main__':
                 is_running = False
             else:
                 print("Your eq was " + str(iSTR))
-                checks = ["+", "**", "//",  "/", "*", "x", "-", "%"]
-                handled = False
-                for check in checks:
-                    if not handled and check in iSTR:
-                        nums = iSTR.split(check)
-                        r = calc.calc(nums[0].strip(), check, nums[1].strip())
-                        print("R is " + str(r))
-                        handled = True
-                if not handled:
-                    print("The action you tried is not supported, please try again")
-    else:  # exit loop
+                if "+" in iSTR:
+                    nums = iSTR.split("+")
+                    r = calc.add(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done before - check to hanlde negative values
+                elif "/" in iSTR:
+                    nums = iSTR.split("/")
+                    r = calc.div(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+                elif "*" in iSTR or "x" in iSTR:
+                    nums = iSTR.split("*") if "*" in iSTR else iSTR.split("x")
+                    r = calc.mult(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done last so negative numbers work
+                elif "-" in iSTR:
+                    nums = iSTR.split("-")
+                    r = calc.sub(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+
+
+
+    else:
         print("Good bye")
         is_running = False
